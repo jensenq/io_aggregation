58355f300fc4ed7b1a3596993536aff05da69b3a
jensenq <jensenq@wwu.edu>
2020-04-23 17:04:28 -0700

    horribly broken, WIP

--- io_intercept.c
+++ io_intercept.c
@@ -8,20 +8,67 @@
 
 char* recover_filename(int fd);
 void log_access(char* fname, char* type, size_t num_bytes);
- 
+void	place_in_buf(void* buf, size_t count);
+
+const int MAX_WRITES = 10; // number of writes before flushing
+const size_t MAX_BUF SIZE = 32000000; //total buffer size before flushing
+const size_t MAX_WRITE_SIZE = MAX_BUF_SIZE/MAX_WRITES; //size in bytes allocated for each write
+
+typedef struct file_buf{
+
+	 char* filename;
+	 char* write_buf[MAX_WRITES]; // buffer where all writes are stored until flushed
+	 int write_index = 0;
+
+} file_buf;
+
+file_buf* file_bufs;
+
+
+/* places the data of a write() into the write buffer */
+void place_in_buf(void* buf, size_t size){
+
+	 if(size<MAX_WRITE_SIZE){
+		  write_buf[write_index] = (char*) buf;
+		  write_index++;
+
+		  if(write_index == MAX_WRITES-1){
+				flush_write_buf();
+		  }
+	 }
+}
+void flush_write_buf(){
+	 FILE *f = fopen(filename, "wb+");
+	 fwrite(write_buf, sizeof(char), sizeof(write_buf), f);
+	 fclose(f);
+	 write_index = 0;
+}
+
+// intercept main, allocate memory for the io buffer
+int main(int argc, char* argv[]){
+	 int (*orig_main)(int argc, char* argv[]) = dlsym(RTLD_NEXT, "main");
+	 
+	 for(int i=0; i>MAX_WRITES; i++){
+		  write_buf[i] = (char*) malloc(MAX_BUF_SIZE);
+	 }
+	 return orig_main(argc, argv);
+}
+/*
 // not necessary if we're recording freads and fwrites
-/*int open(const char *filename, int flags, ...){
+int open(const char *filename, int flags, ...){
 	int (*orig_open)(const char*, int) = dlsym(RTLD_NEXT,"open");
+	printf("%s\n",filename);
 	log_access((char*)filename, "open", 0);
 	return orig_open(filename,flags);
 }
 
 FILE* fopen(const char *filename, const char *mode){
 	FILE* (*orig_fopen)(const char*, const char*) = dlsym(RTLD_NEXT, "fopen");
-	log_access((char*)filename, "open", 0);
+	printf("%s\n",filename);
+	log_access((char*)filename, "fopen", 0);
 	return orig_fopen(filename, mode);
-}*/
-
+}
+*/
 pid_t fork(){
 	pid_t (*orig_fork)() = dlsym(RTLD_NEXT, "fork");
 	//printf("fork intercepted");
@@ -31,7 +78,7 @@ pid_t fork(){
 ssize_t read(int fd, void *buf, size_t count){
 	ssize_t (*orig_read)(int, void*, size_t) = dlsym(RTLD_NEXT, "read");
 	char* fname = recover_filename(fd);
-	log_access(fname, "read", count);
+	//log_access(fname, "read", count);
 	free(fname);
 	return orig_read(fd, buf, count);
 }
@@ -39,7 +86,7 @@ ssize_t read(int fd, void *buf, size_t count){
 size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
 	size_t (*orig_fread)(void *, size_t, size_t, FILE*) = dlsym(RTLD_NEXT, "fread");
 	char *fname = recover_filename(fileno(stream));
-	log_access(fname, "fread", size*nmemb);
+	//log_access(fname, "fread", size*nmemb);
 	free(fname);
 	return orig_fread(ptr, size, nmemb, stream);
 }
@@ -47,7 +94,7 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
 ssize_t write(int fd, const void *buf, size_t count){
 	ssize_t (*orig_write)(int, const void*, size_t) = dlsym(RTLD_NEXT, "write");
 	char *fname = recover_filename(fd);
-	log_access(fname, "write", count);
+	place_in_buf(buf, count)
 	free(fname);
 	return orig_write(fd, buf, count);
 }
@@ -55,11 +102,12 @@ ssize_t write(int fd, const void *buf, size_t count){
 size_t fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream){
 	size_t (*orig_fwrite)(const void*, size_t, size_t, FILE*) = dlsym(RTLD_NEXT, "fwrite");
 	char *fname = recover_filename(fileno(stream));
-	log_access(fname, "fwrite", size*nmemb);
 	free(fname);
 	return orig_fwrite(ptr, size, nmemb, stream);
 }
 
+// gets the original filename from the file descriptor
+// linux only (possibly most unix, untested)
 char* recover_filename(int fd){
 	char fd_path[256];
 	sprintf(fd_path, "/proc/self/fd/%d", fd);
@@ -81,6 +129,4 @@ void log_access(char* fname, char* type, size_t num_bytes){
 	fprintf(f, "filename: %s, type: %s, bytes: %zu\n", fname, type, num_bytes);
 	fclose(f);
 }
-
-
 
