0030e60e87c82d181941ff636b2589a33c093f92
jensenq <jensenq@wwu.edu>
2020-09-30 12:05:22 -0700

    logging and timing

--- io_intercept.c
+++ io_intercept.c
@@ -6,9 +6,10 @@
 #include <string.h>
 #include <sys/types.h>
 #include <stdbool.h> 
+#include <sys/time.h>
 
 #define MAX_BUF_SIZE 32000000 //total buffer size before flushing
-#define DEBUG_ON false
+#define DEBUG_LVL 1
 
 /* ===== STRUCTS ===== */
 
@@ -58,24 +59,25 @@ int append_write(file_buf* fb, const void* buf, size_t size){
 	
 	if(size <= MAX_BUF_SIZE){
 		if(fb->curr_size + size > MAX_BUF_SIZE){
-			if(DEBUG_ON){printf("not enough space remaining. data size: %li. curr_size: %li. max size: %i\n", size, fb->curr_size, MAX_BUF_SIZE);}
+			if(DEBUG_LVL>=2){printf("not enough space remaining. data size: %li. curr_size: %li. max size: %i\n", size, fb->curr_size, MAX_BUF_SIZE);}
 			flush_buf(fb);
 		}
 		
 		memcpy(&fb->write_buf[fb->curr_size], buf, size);
 		fb->curr_size += size;
-		if(DEBUG_ON){printf("write recorded. file: %s, curr_size: %li\n", fb->filename, fb->curr_size);}
+
+		if(DEBUG_LVL>=3){printf("write recorded. file: %s, curr_size: %li\n", fb->filename, fb->curr_size);}
 		return 0;
 	}
 	return -1;
 }
 
 /* creates a new file buffer, and adds it to the global list of file buffers
- *  note: inserted at the head (files recently opened are more likely to be opened soon) 
+ *  note: inserted at the head of the list (files recently opened are more likely to be used soon) 
  */
 void insert_fb(int fd, const char* filename, const char* mode){
 	if(get_fb_by_fd(fd) != NULL){ //already exists
-		if(DEBUG_ON){printf("this file buffer is already in memory");}
+		if(DEBUG_LVL>=1){printf("Error: this file buffer is already in memory");}
 	}
 	else{
 		file_buf* old_head = global_fb_ptr;
@@ -93,15 +95,26 @@ void insert_fb(int fd, const char* filename, const char* mode){
 
 void flush_buf(file_buf* fb){
 
-	if(DEBUG_ON){printf("flushing %li bytes from %s buffer\n", fb->curr_size, fb->filename);}
+	struct timeval begin, end;
+	size_t debug_bytes_written;
+	gettimeofday(&begin, 0);
+	debug_bytes_written = fb->curr_size;
+	if(DEBUG_LVL>=3){printf("flushing %li bytes from %s buffer\n", fb->curr_size, fb->filename);}
+
 	//append the flag so write() is treated as normal
 	strcat(&(fb->write_buf[fb->curr_size]), NO_INTERCEPT_FLAG);
 	write(fb->fd, fb->write_buf, fb->curr_size+sizeof(NO_INTERCEPT_FLAG));
-
-	//do we need to zero out the write buf?
 	memset(fb->write_buf, 0, fb->curr_size);
 	fb->curr_size = 0;
 
+	if(DEBUG_LVL>=1){
+		gettimeofday(&end, NULL);
+		long seconds = end.tv_sec - begin.tv_sec;
+		long microseconds = end.tv_usec - begin.tv_usec;
+		double elapsed = seconds + microseconds*1e-6;
+		double mbytes_per_sec = (debug_bytes_written/elapsed)/1000000;
+		printf("Time to flush %ld bytes: %.3f seconds. (%.3f MB/sec) \n", debug_bytes_written, elapsed, mbytes_per_sec);
+	}
 }
 
 
@@ -131,7 +144,7 @@ size_t fwrite(const void* ptr, size_t size, size_t nmemb, FILE* stream){
 	else{	
 		int tmp = append_write(fb, ptr, nmemb);
 		if(tmp == -1){ // write too big for buffer
-			if(DEBUG_ON){printf("data too large. data size: %li. max size: %i\n", nmemb, MAX_BUF_SIZE);}
+			if(DEBUG_LVL>=3){printf("data too large. data size: %li. max size: %i\n", nmemb, MAX_BUF_SIZE);}
 		   return orig_fwrite(ptr, size, nmemb, stream);	  	 
 		}
 	}
@@ -142,7 +155,7 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream){
 	size_t (*orig_fread)(void *, size_t, size_t, FILE*) = dlsym(RTLD_NEXT, "fread");
 	int fd = fileno(stream);
 	file_buf* fb = get_fb_by_fd(fd);
-	if(DEBUG_ON){printf("reading %s\n", fb->filename);}
+	if(DEBUG_LVL>=3){printf("reading %s\n", fb->filename);}
 	flush_buf(fb);
 	return orig_fread(ptr, size, nmemb, stream);
 }
@@ -151,9 +164,9 @@ int fclose(FILE* stream){
 	int (*orig_fclose)(FILE*) = dlsym(RTLD_NEXT, "fclose");
 	int fd = fileno(stream);
 	file_buf* fb = get_fb_by_fd(fd);
-	if(DEBUG_ON){printf("closing %s\n", fb->filename);}
+	if(DEBUG_LVL>=3){printf("closing %s\n", fb->filename);}
 	flush_buf(fb);
-	if(DEBUG_ON){printf("%s closed.\n", fb->filename);}
+	if(DEBUG_LVL>=3){printf("%s closed.\n", fb->filename);}
 	delete_fb(fb);
 	return orig_fclose(stream); 
 }
@@ -168,7 +181,7 @@ int open(const char *filename, int flags, ...){
 ssize_t read(int fd, void *buf, size_t count){
 	ssize_t (*orig_read)(int, void*, size_t) = dlsym(RTLD_NEXT, "read");
 	file_buf* fb = get_fb_by_fd(fd);
-	if(DEBUG_ON){printf("reading %s\n", fb->filename);}
+	if(DEBUG_LVL>=3){printf("reading %s\n", fb->filename);}
 	flush_buf(fb);
 	return orig_read(fd, buf, count);
 }
@@ -184,7 +197,7 @@ ssize_t write(int fd, const void *buf, size_t count){
 	else{
 		int tmp = append_write(fb, buf, count);
 		if(tmp == -1){ // write too big for buffer
-			if(DEBUG_ON){printf("data too large, writing to disk... data size: %li. max size: %i\n", count, MAX_BUF_SIZE);}
+			if(DEBUG_LVL>=3){printf("data too large, writing to disk... data size: %li. max size: %i\n", count, MAX_BUF_SIZE);}
 			return orig_write(fd, buf, count);
 		}
 	}
@@ -219,7 +232,7 @@ bool treat_as_normal(const void* void_buf, size_t bufsize, file_buf* fb){
 	if(strcmp(tmp_flag, NO_INTERCEPT_FLAG) == 0){	
 		return true;
 	}
-	//if(DEBUG_ON){printf("FLAG FAILED: \"%s\" \"%s\"\n", tmp_flag, NO_INTERCEPT_FLAG);}
+	//if(DEBUG_LVL){printf("FLAG FAILED: \"%s\" \"%s\"\n", tmp_flag, NO_INTERCEPT_FLAG);}
 	return false;
 }
 
