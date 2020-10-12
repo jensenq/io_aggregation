62c5155e449f3c06eeed5d411cee08bd8545a178
jensenq <jensenq@wwu.edu>
2020-10-11 22:06:05 -0700

    bugfix perf measurements

--- perf.sh
+++ perf.sh
@@ -5,13 +5,16 @@ numwrites=10
 size=1000
 
 #numwrites
-echo -e "real\ttime\tnumwrites" >> time_vs_numwrites.log
+echo -e "real\ttime    \tnumwrites\tsize" >> time_vs_numwrites.log
 for (( i=0; i<num_data_pts; i++)); do
-	#{ time LD_PRELOAD=./io_intercept.so ./test 1 1; } 2>&1 | grep real >> time_vs_numwrites.log
 	time=$({ time LD_PRELOAD=./io_intercept.so ./test $numwrites $size; }  2>&1 | grep real)
-	echo -e "$time\t$numwrites \n" >> time_vs_numwrites.log
+	echo -e "$time\t$numwrites\t$size" >> time_vs_numwrites.log
 	numwrites=$(( numwrites * 10 ))
 done
+
+
+
+#{ time LD_PRELOAD=./io_intercept.so ./test 1 1; } 2>&1 | grep real >> time_vs_numwrites.log
 	 
 	
 
