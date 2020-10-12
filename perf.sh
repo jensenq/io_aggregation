8b73af8e4c0d9fec6e0d239006b2cd88a7817d09
jensenq <jensenq@wwu.edu>
2020-10-11 22:16:55 -0700

    tweak

--- perf.sh
+++ perf.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 num_data_pts=10
-numwrites=10
-size=1000
+numwrites=2
+size=1000 #1000 is about the median size of pipeline writes
 
-#numwrites
+#numwrites - caps at ~1M writes
 echo -e "real\ttime    \tnumwrites\tsize" >> time_vs_numwrites.log
 for (( i=0; i<num_data_pts; i++)); do
 	time=$({ time LD_PRELOAD=./io_intercept.so ./test $numwrites $size; }  2>&1 | grep real)
 	echo -e "$time\t$numwrites\t$size" >> time_vs_numwrites.log
-	numwrites=$(( numwrites * 10 ))
+	numwrites=$(( numwrites * 2 ))
 done
 
 
